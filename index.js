d74fc26713c380009b5ba43d676c333906071556
Angel <rodriguezangelr97@gmail.com>
2021-03-21 15:28:15 -0400

    Completed Array Lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+beforeEach(function () {
+    cats.lenth = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+});
+
+
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+    destructivelyAppendCat('Ralph');
+    
+    destructivelyAppendCat('Ralph');
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
